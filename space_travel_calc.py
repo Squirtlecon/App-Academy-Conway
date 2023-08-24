ef599edac2fa3743e7f6f8ba34464ec62be91e46
Conway Lau <squirtlecon123@gmail.com>
2023-08-24 17:32:19 +0000

    message

---
+++ space_travel_calc.py
@@ -0,0 +1,5 @@
+distance = float(input("how far are you going (in lightyears)"))
+speed = float(input("what speed are you going?"))
+time = distance/speed
+print('It will take you', time, "to get there")
+5
